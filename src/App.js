5d47fe374d48a2261ac988b71d530fb109a8ddc9
Haeparic <v567v202@gmail.com>
2022-12-16 15:45:22 +0900

    경로수정

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Header from "./components/Header";
-import Booklist from "./pages/Booklist";
-import Bookdetail from "./pages/Bookdetail";
+import Booklist from "./components/Booklist";
+import Bookdetail from "./components/Bookdetail";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 const App = () => {
---
+++ src/components/Header.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <>
+      <header id="masthead" class="site-header">
+        <div class="site-branding">
+          <h1 class="site-title">
+            <a href="index.html" rel="home">
+              Moschino
+            </a>
+          </h1>
+          <h2 class="site-description">Minimalist Portfolio HTML Template</h2>
+        </div>
+      </header>
+    </>
+  );
+};
+
+export default Header;
