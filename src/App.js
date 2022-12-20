7348065929d055ac454e14a0329b05a9dc848060
Haeparic <v567v202@gmail.com>
2022-12-20 14:30:16 +0900

    about 중간완성

--- src/App.js
+++ src/App.js
@@ -1,27 +1,27 @@
 import Header from "./components/Header";
 import Home from "./components/Home";
+import About from "./components/About";
 import Booklist from "./components/Booklist";
 import Bookdetail from "./components/Bookdetail";
 import Footer from "./components/Footer";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import { useEffect, useState } from "react";
 import instance from "./api/axios";
 import request from "./api/request";
 
 const App = () => {
-
   const [book, setBook] = useState({});
 
   const params = {
-    Seq: 1
+    Seq: 1,
   };
 
   const fetchData = async () => {
     const resultBook = await instance.get(request.fetchBook, { params });
-    setBook (resultBook.data.list[0])
-  }
+    setBook(resultBook.data.list[0]);
+  };
 
-  useEffect( () => {
+  useEffect(() => {
     fetchData();
   }, []);
 
@@ -29,14 +29,15 @@ const App = () => {
     <Router basename={process.env.PUBLIC_URL}>
       <Header />
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Navigate to="/home" />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/about" element={<About />} />
         <Route path="/booklist" element={<Booklist />} />
-        <Route path="/bookdetail" element={<Bookdetail book={book}/>} />
+        <Route path="/bookdetail" element={<Bookdetail book={book} />} />
       </Routes>
       <Footer />
     </Router>
   );
 };
-
 
 export default App;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,19 +1,20 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
     <div className="container">
       <div
         id="carouselExampleInterval"
-        class="carousel slide carousel-fade"
+        className="carousel slide carousel-fade mt-3 mb-5"
         data-bs-ride="carousel"
       >
-        <div class="carousel-indicators">
+        <div className="carousel-indicators">
           <button
             type="button"
             data-bs-target="#carouselExampleIndicators"
             data-bs-slide-to="0"
-            class="active"
+            className="active"
             aria-current="true"
             aria-label="Slide 1"
           ></button>
@@ -30,9 +31,9 @@ const Home = () => {
             aria-label="Slide 3"
           ></button>
         </div>
-        <div class="carousel-inner pt-200">
-          <div class="carousel-item  active" data-bs-interval="7000">
-            <h3 className="carousel-caption position-absolute text-end">
+        <div className="carousel-inner pt-200">
+          <div className="carousel-item  active" data-bs-interval="7000">
+            <h3 className="carousel-caption position-absolute text-end text-3xl">
               주도적인 삶을 살기 위한 <br />
               3단계 핵심 솔루션
             </h3>
@@ -42,8 +43,8 @@ const Home = () => {
               alt="banner1"
             />
           </div>
-          <div class="carousel-item">
-            <h3 className="carousel-caption position-absolute text-end">
+          <div className="carousel-item">
+            <h3 className="carousel-caption position-absolute text-end text-3xl">
               새로운 관점으로 <br />
               우리의 삶을 위로한다
             </h3>
@@ -53,8 +54,8 @@ const Home = () => {
               alt="banner2"
             />
           </div>
-          <div class="carousel-item">
-            <h3 className="carousel-caption position-absolute text-end">
+          <div className="carousel-item">
+            <h3 className="carousel-caption position-absolute text-end text-3xl">
               새로운 언어는 <br />
               새로운 시대를 반영한다
             </h3>
@@ -66,74 +67,171 @@ const Home = () => {
           </div>
         </div>
         <button
-          class="carousel-control-prev"
+          className="carousel-control-prev"
           type="button"
           data-bs-target="#carouselExampleInterval"
           data-bs-slide="prev"
         >
-          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-          <span class="visually-hidden">Previous</span>
+          <span
+            className="carousel-control-prev-icon"
+            aria-hidden="true"
+          ></span>
+          <span className="visually-hidden">Previous</span>
         </button>
         <button
-          class="carousel-control-next"
+          className="carousel-control-next"
           type="button"
           data-bs-target="#carouselExampleInterval"
           data-bs-slide="next"
         >
-          <span class="carousel-control-next-icon" aria-hidden="true"></span>
-          <span class="visually-hidden">Next</span>
+          <span
+            className="carousel-control-next-icon"
+            aria-hidden="true"
+          ></span>
+          <span className="visually-hidden">Next</span>
         </button>
       </div>
-      <div class="container">
-        <div class="section-title">
-          <h2>오늘의 선택</h2>
+      <div className="container home-book mb-5">
+        <div className="section-title d-flex justify-content-between m-3">
+          <h2 className="text-3xl font-medium"># 오늘의 선택</h2>
+          <Link
+            className="btn text-base align-self-center home-book-link"
+            to={"/booklist"}
+            role="button"
+          >
+            MORE
+          </Link>
         </div>
-        <div class="card-group">
-          <div class="card">
-            <img
-              src={process.env.PUBLIC_URL + "/images/book1.jpg"}
-              class="card-img-top"
-              alt="..."
-            />
-            <div class="card-body">
-              <h5 class="card-title">책 제목1</h5>
-              <span class="card-text">작가1</span>
+        <hr />
+        {/* <div className="container mb-5">
+          <div className="row row-cols-1 row-cols-sm-2 row-cols-md-4">
+            <div className="col">
+              <div className="card h-100">
+                <img
+                  src={process.env.PUBLIC_URL + "/images/book1.jpg"}
+                  className="card-img-top"
+                  alt="book1"
+                />
+                <div className="card-body">
+                  <h5 className="card-title">책 제목1</h5>
+                  <p className="card-text">작가1</p>
+                </div>
+              </div>
+            </div>
+            <div className="col">
+              <div className="card h-100">
+                <img
+                  src={process.env.PUBLIC_URL + "/images/book2.jpg"}
+                  className="card-img-top"
+                  alt="book2"
+                />
+                <div className="card-body">
+                  <h5 className="card-title">책 제목2</h5>
+                  <p className="card-text">작가2</p>
+                </div>
+              </div>
+            </div>
+            <div className="col">
+              <div className="card h-100">
+                <img
+                  src={process.env.PUBLIC_URL + "/images/book3.jpg"}
+                  className="card-img-top"
+                  alt="book3"
+                />
+                <div className="card-body">
+                  <h5 className="card-title">책 제목3</h5>
+                  <p className="card-text">작가3</p>
+                </div>
+              </div>
+            </div>
+            <div className="col">
+              <div className="card h-100">
+                <img
+                  src={process.env.PUBLIC_URL + "/images/book4.jpg"}
+                  className="card-img-top"
+                  alt="book4"
+                />
+                <div className="card-body">
+                  <h5 className="card-title">책 제목4</h5>
+                  <p className="card-text">작가4</p>
+                </div>
+              </div>
             </div>
           </div>
-          <div class="card">
-            <img
-              src={process.env.PUBLIC_URL + "/images/book2.jpg"}
-              class="card-img-top"
-              alt="..."
-            />
-            <div class="card-body">
-              <h5 class="card-title">책 제목1</h5>
-              <span class="card-text">작가1</span>
-            </div>
-          </div>
-          <div class="card">
-            <img
-              src={process.env.PUBLIC_URL + "/images/book3.jpg"}
-              class="card-img-top"
-              alt="..."
-            />
-            <div class="card-body">
-              <h5 class="card-title">책 제목1</h5>
-              <span class="card-text">작가1</span>
-            </div>
-          </div>
-          <div class="card">
-            <img
-              src={process.env.PUBLIC_URL + "/images/book4.jpg"}
-              class="card-img-top"
-              alt="..."
-            />
-            <div class="card-body">
-              <h5 class="card-title">책 제목1</h5>
-              <span class="card-text">작가1</span>
-            </div>
-          </div>
-        </div>
+        </div> */}
+        <ul class="grid gap-4 mt-8 sm:grid-cols-2 lg:grid-cols-4">
+          <li>
+            <a href="#" class="block overflow-hidden group">
+              <img
+                src={process.env.PUBLIC_URL + "/images/book1.jpg"}
+                alt=""
+                class="h-[250px] w-full object-contain transition duration-500 group-hover:scale-105 sm:h-[350px]"
+              />
+              <div class="relative pt-3 bg-white">
+                <p>
+                  <span class="tracking-wider text-gray-900 group-hover:underline group-hover:underline-offset-4"> 제목 1 </span>
+                </p>
+                <span class="text-xs text-gray-700">
+                  작가 1
+                </span>
+              </div>
+            </a>
+          </li>
+
+          <li>
+            <a href="#" class="block overflow-hidden group">
+              <img
+                src={process.env.PUBLIC_URL + "/images/book2.jpg"}
+                alt=""
+                class="h-[250px] w-full object-contain trnansition duration-500 group-hover:scale-105 sm:h-[350px]"
+              />
+              <div class="relative pt-3 bg-white">
+                <p>
+                  <span class="tracking-wider text-gray-900 group-hover:underline group-hover:underline-offset-4"> 제목 2 </span>
+                </p>
+                <span class="text-xs text-gray-700">
+                  작가 2
+                </span>
+              </div>
+            </a>
+          </li>
+
+          <li>
+            <a href="#" class="block overflow-hidden group">
+              <img
+                src={process.env.PUBLIC_URL + "/images/book3.jpg"}
+                alt=""
+                class="h-[250px] w-full object-contain transition duration-500 group-hover:scale-105 sm:h-[350px]"
+              />
+              <div class="relative pt-3 bg-white">
+                <p>
+                  <span class="tracking-wider text-gray-900 group-hover:underline group-hover:underline-offset-4"> 제목 3 </span>
+                </p>
+                <span class="text-xs text-gray-700">
+                  작가 3
+                </span>
+              </div>
+            </a>
+          </li>
+
+          <li>
+            <a href="#" class="block overflow-hidden group">
+              <img
+                src={process.env.PUBLIC_URL + "/images/book4.jpg"}
+                alt=""
+                class="h-[250px] w-full object-contain transition duration-500 group-hover:scale-105 sm:h-[350px]"
+              />
+              <div class="relative pt-3 bg-white">
+                <p>
+                  <span class="tracking-wider text-gray-900 group-hover:underline group-hover:underline-offset-4"> 제목 4 </span>
+                </p>
+                <span class="text-xs text-gray-700">
+                  작가 4
+                </span>
+              </div>
+            </a>
+          </li>
+        </ul>
       </div>
     </div>
   );
